c09899998a88b161c13c09256fa8a0e7b785363b
basavaraj16 <basavarajmugalkhod2005@gmail.com>
2025-06-11 06:49:49 +0530

    age chek

---
+++ Agecheck.java
@@ -0,0 +1,22 @@
+
+public class Agecheck{
+    
+
+    public static void main(String[] args) {
+        Agecheck Ac = new Agecheck();
+        
+         
+       
+            int age = 16;
+
+    if(age>18){
+        System.out.println("Eligible to vote");
+    }else{
+        System.out.println("not Eligible to vote");
+    }
+
+  
+    }
+}
+
+
